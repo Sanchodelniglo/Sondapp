9f6620261e2a11e78c73fe698110777ec1fefecb
Romain Grossard <rgrossard@gmail.com>
2018-07-18 15:40:12 +0200

    created CRUD + views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :password])
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+      user_params.permit(:first_name, :last_name, :email, :genre, :age, :phone_number, :pathology)
+    end
+  end
 end
---
+++ app/controllers/probings_controller.rb
@@ -0,0 +1,33 @@
+class ProbingsController < ApplicationController
+
+
+  def index
+    @probings = Probing.all
+  end
+
+  def last
+    @probings = Probing.last(6)
+
+  end
+
+  def new
+    @probing = Probing.new
+  end
+
+  def create
+    @probing = Probing.new(probing_params)
+    @probing.user = current_user
+    if @probing.save
+      redirect_to probings_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def probing_params
+    params.require(:probing).permit(:hydratation, :quantity, :quality, :fleed)
+  end
+
+end
