bf4a0296676720675ad684c2bc382ba2cd43697d
Romain Grossard <rgrossard@gmail.com>
2018-08-09 12:11:38 +0200

    add pdf but chart doesn't display

--- app/controllers/probings_controller.rb
+++ app/controllers/probings_controller.rb
@@ -48,10 +48,52 @@ class ProbingsController < ApplicationController
   end
 
   def show
+
+    cookies[:date] = DateTime.now
+    @probings = Probing.where(user_id: current_user).last(6)
+    @chart = LazyHighCharts::HighChart.new('graph') do |f|
+      f.title(text: "du #{@probings.pluck(:created_at).map { |date| date.strftime("%d/%m/%Y") }.uniq.join(' au ')}")
+      f.xAxis(
+        categories: @probings.pluck(:created_at).map { |date| date.strftime("%H:%M") },
+        plotBands: probing_quality(@probings)
+      )
+
+      f.yAxis [
+        {title: {text: "Volume en cl"} },
+        {title: {text: "Fuites urinaire en cl"}, opposite: true, allowDecimals: false}]
+      f.series(type: 'column', name: "Fuites", yAxis: 1, data: @probings.pluck(:fleed), maxPointWidth: 20)
+      f.series(type: 'spline', name: "Boisson", yAxis: 0, data: @probings.pluck(:hydratation))
+      f.series(type: 'spline', name: "Miction", yAxis: 0, data: @probings.pluck(:quantity))
+
+
+
+      f.legend(align: 'center', verticalAlign: 'bottom', y: 0, x: 0, layout: 'horizontal')
+      f.export(:type=> 'image/pdf')
+      f.global(useUTC: false)
+      f.chart(
+        backgroundColor: {
+          linearGradient: [0, 0, 500, 500],
+          strokeWidth: 0,
+          stops: [
+            [0, "rgb(255, 255, 255)"],
+            [1, "rgb(240, 240, 255)"]
+          ]
+        },
+        borderWidth: 2,
+        plotBackgroundColor: "white",
+        plotShadow: true,
+        plotBorderWidth: 0,
+        animation: false,
+        enableMouseTracking: false,
+        shadow: false,
+      )
+      f.lang(thousandsSep: ",", numericSymbols: 'cl')
+      f.colors(["#9feed1", "#53c7f0", "#f8c43a", "#fff6a2", "#e4d354"])
+    end
     respond_to do |format|
       format.html { render :show }
       format.pdf {
-        render :pdf => "show", :layout => 'charts_pdf.html'
+        render :pdf => "show", :layout => 'probings_pdf.html'
         }
     end
   end
@@ -98,5 +140,4 @@ class ProbingsController < ApplicationController
     end
     bad_probings.flatten
   end
-
 end
