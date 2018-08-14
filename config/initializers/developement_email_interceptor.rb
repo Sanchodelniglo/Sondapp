1f4a8b7ae178a368224314b20971227fcb039d49
charles-henri poniard <c.poniard@gmail.com>
2018-08-14 13:16:30 +0200

    add interceptor

--- config/initializers/developement_email_interceptor.rb
+++ config/initializers/developement_email_interceptor.rb
@@ -1,9 +1,9 @@
-# if Rails.env.development?
-#   require "email_interceptor"
-#   ActionMailer::Base.register_interceptor(EmailInterceptor)
-# end
+if Rails.env.development?
+  require "email_interceptor"
+  ActionMailer::Base.register_interceptor(EmailInterceptor)
+end
 
-# if Rails.env.production?
-#   require "email_interceptor"
-#   ActionMailer::Base.register_interceptor(EmailInterceptor)
-# end
+if Rails.env.production?
+  require "email_interceptor"
+  ActionMailer::Base.register_interceptor(EmailInterceptor)
+end
