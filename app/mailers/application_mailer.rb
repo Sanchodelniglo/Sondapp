a6e7a9add4fd6d549879909acc92c4e2d18289b7
charles-henri poniard <c.poniard@gmail.com>
2018-08-14 11:10:15 +0200

    test 2.0 mail prod

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'c.poniard@gmail.com'
-  layout 'mailer'
+  # default from: 'c.poniard@gmail.com'
+  # layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: "sond-app.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "sond-app.herokuapp.com", port: 80 }
 
   # Setup the mailer config
   config.action_mailer.delivery_method = :smtp
